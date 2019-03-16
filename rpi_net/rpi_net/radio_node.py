22de6971b61961b230bccff7e7f279fc00ad46cd
Alex Elias <aelias36@yahoo.com>
2019-03-16 18:21:41 -0400

    added stubbed version for radio_node.py

--- rpi_net/rpi_net/radio_node.py
+++ rpi_net/rpi_net/radio_node.py
@@ -1,5 +1,7 @@
 import asyncio
 
+import random
+
 class RadioNode():
     def __init__(self, main_server):
         self.main_server = main_server
@@ -8,4 +10,11 @@ class RadioNode():
         while True:
             pass
             # Listens for updates on radio
-            # self.main_server.set_states(states)
+            # self.main_server.set_room_state(states)
+
+    async def run_stubbed(self):
+    	while True:
+    		await asyncio.sleep(1)
+    		randint(1, 100)
+    		fakeState = {"roomID" : randint(1, 5), "occupied" : randint(0,1)}
+    		self.main_server.set_room_state(fakeState)
