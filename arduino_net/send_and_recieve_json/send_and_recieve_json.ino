ca6b83ad021ef51dfbfc3029743d8d4de53f1c0c
jjhumphreys11 <jj.humphreys11@gmail.com>
2019-03-17 00:29:56 -0400

    added noise sensor functionality, rolling averages, simpler json structure

--- arduino_net/send_and_recieve_json/send_and_recieve_json.ino
+++ arduino_net/send_and_recieve_json/send_and_recieve_json.ino
@@ -3,9 +3,19 @@
 #include <RF24.h>
 #include <ArduinoJson.h>
 
-const byte read_address[3][6] = { "00001", "00003", "00004" };
-const int read_num = 3;
-const byte write_address[6] = "00002";
+#define ROOM_ID 23
+#define NUM_INPUTS 0
+
+const byte write_address[6] = "00001";
+const byte read_address[6][6] = { "00001", "00003", "00004" };
+
+
+int noise[100];
+int motion[100];
+int sample = 0;
+
+float av_noise = 0.0;
+float av_motion = 0.0;
 
 RF24 radio(7, 8);  
 
@@ -21,7 +31,7 @@ RF24 radio(7, 8);
 //   IRQ  :  No Connection
 
 int input_num = 0;
-char input_json[3][256];
+char input_json[3][32];
 int num = 1;
 
 const size_t capacity = JSON_OBJECT_SIZE(4);
@@ -30,14 +40,18 @@ DynamicJsonDocument doc(capacity);
 int noisePin = 5;
 int motionPin = 6;
 
-// const uint8_t* json = "{\"roomID\":23,\"occupied\":1}";
-char this_json[256];
+char this_json[32];
 
 void setup() {
     Serial.begin(9600);
     
     pinMode(noisePin, INPUT);
     pinMode(motionPin, INPUT);
+    for(int r = 0; r < 100; r++)
+    {
+      noise[r] = 0;
+      motion[r] = 0;
+    }
     
     radio.begin();
     radio.openWritingPipe(write_address);
@@ -45,10 +59,9 @@ void setup() {
     radio.setPALevel(RF24_PA_MIN);
     //DynamicJsonDocument doc_in(capacity);
 
-    doc["roomID"] = 56;
-    doc["occupied"] = 1;
-    doc["noise"] = 0;
-    doc["motion"] = 0;
+    doc["r"] = ROOM_ID;
+    doc["c"] = 0;
+    doc["n"] = 0;
 }
 
 void loop() {
@@ -59,28 +72,58 @@ void loop() {
     input_num = 0;
     while(radio.available())
     {
-      radio.read(input_json[input_num++], 255);
-      if(input_num >= read_num) break;
+      radio.read(input_json[input_num++], 32);
+      if(input_num >= NUM_INPUTS) break;
     }
   }
 
-  delay(100);
+  //delay(10);
   radio.stopListening();
   // write info for current arduino
   // write info for other arduinos
-  doc["noise"] = digitalRead(noisePin);
-  doc["motion"] = digitalRead(motionPin);
+  for(int e = 0; e < 100; e++)
+  {
+    noise[sample%100] = digitalRead(noisePin);
+    if(noise[sample%100] > 0) break;
+    delay(1);
+  }
+  //noise[sample%100] = digitalRead(noisePin);
+  motion[sample++%100] = digitalRead(motionPin);
+  av_noise = 0.0;
+  av_motion = 0.0;
+  for(int t = 0; t < 100; t++)
+  {
+    av_noise = av_noise + noise[t];
+    av_motion = av_motion + motion[t];
+  }
+  doc["n"] = av_noise;
+  if(av_motion >= 10)
+  {
+    doc["c"] = 1;
+  }
+  else
+  {
+    if(av_noise >= 50)
+    {
+      doc["c"] = 1;
+    }
+    else
+    {
+      doc["c"] = 0;
+    }
+  }
   
   serializeJson(doc, this_json);
-  Serial.println("====Self====");
   radio.write(this_json, strlen(this_json));
   Serial.println(this_json);
 
-  Serial.println("===inputs====");
-  for(int t = 0; t < read_num; t++)
+  for(int t = 0; t < NUM_INPUTS; t++)
   {
-    radio.write(input_json[t], strlen(input_json[t]));
-    Serial.println(input_json[t]);
+    if(input_json[t] != "\0")
+    {
+      radio.write(input_json[t], strlen(input_json[t]));
+      Serial.println(input_json[t]);
+    }
   }
   
 }
