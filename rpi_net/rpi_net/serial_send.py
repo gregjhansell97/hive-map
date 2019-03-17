036782b488e9d1a6d6b90ce4ed6c96a484136c02
Alex Elias <aelias36@yahoo.com>
2019-03-16 23:42:15 -0400

    deleted radio_test, updated serial_send

--- rpi_net/rpi_net/serial_send.py
+++ rpi_net/rpi_net/serial_send.py
@@ -1,4 +1,21 @@
 import serial
-ser = serial.Serial("/dev/tty.usbserial", 9600)
+
+import serial.tools.list_ports
+ports = list(serial.tools.list_ports.comports())
+print("---------- available serial ports ---------- ")
+for p in ports:
+    print(p)
+print("-------------------------------------------- ")
+
+ser = None
+for p in ports:
+	if "Arduino" in p.description:
+		print("opening port")
+		print(p.device)
+		ser = serial.Serial(p.device, 9600)
+		break
+if ser == None:
+	raise exception("No Arduino found!")
+
 while True:
     print(ser.readline())
