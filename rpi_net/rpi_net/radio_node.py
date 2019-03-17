29e1a40433798d2a1f51b6bd1eac495e85c868f0
Alex Elias <aelias36@yahoo.com>
2019-03-16 21:04:35 -0400

    Update radio_node.py

--- rpi_net/rpi_net/radio_node.py
+++ rpi_net/rpi_net/radio_node.py
@@ -6,11 +6,36 @@ class RadioNode():
     def __init__(self, main_server):
         self.main_server = main_server
 
+        self.json_map = 
+        {
+            "c" : "occupied",
+            "n" : "noiseLevel"
+        }
+
     async def run(self):
         while True:
             pass
             # Listens for updates on radio
             # self.main_server.set_room_state(states)
+
+            A_layer_json = {"r" : randint(1,5), "c": randint(1,5), "n": randint(1,5)}
+
+            if "r" in A_layer_json.keys():
+                dynamic_props = {}
+
+                for A_layer_keyname in self.json_map.keys():
+                    if A_layer_keyname in A_layer_json.keys():
+                        dynamic_props[self.json_map[A_layer_keyname]] = A_layer_json[A_layer_keyname]
+
+                state = {
+                        A_layer_json[r]:
+                            {
+                            "dynamic_props": dynamic_props
+                            }
+                    }
+                await self.main_server.set_room_state(state)
+
+
 
     async def run_stubbed(self):
     	while True:
