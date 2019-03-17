68ab590c4952cd4d78d9b2bf7e96d2749720a550
Chris <crs4263@rit.edu>
2019-03-17 00:07:50 -0400

    Getting Serial to work with Ubuntu and Windows will be fun x.x

--- rpi_net/rpi_net/radio_node.py
+++ rpi_net/rpi_net/radio_node.py
@@ -2,6 +2,9 @@ import asyncio
 
 from random import randint
 
+import serial
+from serial.tools import list_ports
+
 class RadioNode():
     def __init__(self, main_server):
         self.main_server = main_server
@@ -14,34 +17,42 @@ class RadioNode():
     async def run(self):
         while True:
             await asyncio.sleep(0.1)
-            # Listens for updates on radio
-            # self.main_server.set_room_state(states)
 
-            A_layer_json = {"r" : "1", "c": randint(1,5), "n": randint(1,5)}
+            ports = list(list_ports.comports())
+            ser = None
+            for p in ports:
+                if "Arduino" in p.description:
+                    ser = serial.Serial(p.device, 9600)
+                    break
+            if ser == None:
+                if ports:
+                    ser == serial.Serial(ports[0].device, 9600)
+            if ser is not None:
+                A_layer_json = ser.readline()
+                print(A_layer_json)
+                if "r" in A_layer_json.keys():
+                    dynamic_props = {}
 
-            if "r" in A_layer_json.keys():
-                dynamic_props = {}
+                    for A_layer_keyname in self.json_map.keys():
+                        if A_layer_keyname in A_layer_json.keys():
+                            dynamic_props[self.json_map[A_layer_keyname]] = A_layer_json[A_layer_keyname]
 
-                for A_layer_keyname in self.json_map.keys():
-                    if A_layer_keyname in A_layer_json.keys():
-                        dynamic_props[self.json_map[A_layer_keyname]] = A_layer_json[A_layer_keyname]
-
-                state = {
-                        A_layer_json['r']:
-                            {
-                            "dynamic_props": dynamic_props
-                            }
-                    }
-                await self.main_server.set_room_state(state)
+                    state = {
+                            A_layer_json['r']:
+                                {
+                                "dynamic_props": dynamic_props
+                                }
+                        }
+                    await self.main_server.set_room_state(state)
 
 
 
     async def run_stubbed(self):
-    	while True:
-    		await asyncio.sleep(0.1)
-    		fakeState = {randint(1,5): {
+        while True:
+            await asyncio.sleep(0.1)
+            fakeState = {randint(1,5): {
                 "dynamic_props":{
                     "occupied" : randint(0,1)}
                 }
             }
-    		await self.main_server.set_room_state(fakeState)
+            await self.main_server.set_room_state(fakeState)
